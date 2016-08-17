582fc2f45bf7138d32f887a1bed6d46f08c102ad
Ryan Kliemann <Ryan_blake@icloud.com>
2016-08-17 17:29:00 -0500

    update dataService as a factory

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -3,15 +3,16 @@
       .module("factQuiz")
       .controller("quizCtrl", QuizController);
 
-      QuizController.$inject = ['quizMetrics'];
+      QuizController.$inject = ['quizMetrics', 'DataService'];
 
-      function QuizController(quizMetrics){
+      function QuizController(quizMetrics, DataService){
         var vm = this;
 
         vm.quizMetrics = quizMetrics;
-
-
+        // vm.dataService = DataService;
       }
+
+
 
 
 })();
