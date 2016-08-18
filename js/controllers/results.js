1ca83a4673270b3264112b3170be275bc6c84cd1
Ryan Kliemann <Ryan_blake@icloud.com>
2016-08-18 13:36:31 -0500

    style correct and wrong answers and return to facts btn

--- js/controllers/results.js
+++ js/controllers/results.js
@@ -10,7 +10,23 @@
       vm.quizMetrics = quizMetrics;
       vm.dataService = DataService;
       vm.getAnswerClass = getAnswerClass;
+      vm.setActiveQuestion = setActiveQuestion;
+      vm.caclulatePerc = calculatePerc;
+      vm.reset = reset;
       vm.activeQuestion = 0;
+
+
+
+      function calculatePerc(){
+        return results.quizMetrics.numCorrect / results.dataService.quizQuestions.length * 100 ;
+      }
+
+
+      function setActiveQuestion(index){
+         vm.activeQuestion = index;
+
+      }
+
 
       function getAnswerClass(index){
          if(index === quizMetrics.correctAnswers[vm.activeQuestion]){
@@ -18,8 +34,19 @@
          } else if (index === DataService.quizQuestions[vm.activeQuestion].selected){
            return "bg-danger";
          }
-
       };
+
+      function reset(){
+          quizMetrics.changeState("results", false);
+          quizMetrics.numCorrect = 0;
+
+          for(var i =0; i < DataService.quizQuestions.length; i++){
+            var data = DataService.quizQuestions[i];
+
+            data.selected = null;
+            data.correct = null;
+          }
+      }
 
     }
 
